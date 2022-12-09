eb168d4c768bf7b8569edfc03285f896a5b68925
ЕрмолаевАС <@101-03>
2022-12-09 13:22:35 +0300

    asd

--- keyR/Header.h
+++ keyR/Header.h
@@ -1,3 +1,13 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
 #include <Windows.h>
-#include <winreg.h>
+#include <stdio.h>
+
+HANDLE hConsole;
+
+
+void GetSettings();
+void SetFontSize();
+void SetFontColor();
+void SetBackground();
+void SetFontType();
--- keyR/Source.c
+++ keyR/Source.c
@@ -1,23 +1,56 @@
 #include "Header.h"
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
-	HKEY hkey;
-	HKEY hMyKey;
-	RegOpenKeyW(HKEY_CURRENT_USER, NULL, &hkey);
-	if (RegCreateKeyW(hkey, L"MyKey", &hMyKey) == ERROR_SUCCESS)
-	{
-		//MessageBoxW(NULL, L"Раздел создан", L"YES", MB_OK);
-	}
-	if (RegSetValueW(hMyKey, NULL, REG_SZ, L"Message", 8 * sizeof(WCHAR)) == ERROR_SUCCESS)
-	{
-		WCHAR text [256];
-		DWORD size = sizeof(WCHAR) * 256;
-		if (RegGetValueW(hMyKey, NULL, NULL, RRF_RT_REG_SZ, NULL, text,&size ) == ERROR_SUCCESS)
-		{
-			MessageBoxW(NULL, text, L"YES", MB_OK);
-		}
+main() {
+	system("chcp 1251>nul");
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO csb;
+	GetConsoleScreenBufferInfo(hConsole, &csb);
+	printf("%s", "aboba\n");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
+	SHORT a = 12;
+	COORD CORD = { a,a };
+	csb.dwSize = CORD;
+	printf("%s", "aboba\n");
+	SetConsoleTextAttribute(hConsole, csb.wAttributes);
+	//HKEY hkey;
+	//HKEY hMyKey;
+	//RegOpenKeyW(HKEY_CURRENT_USER, NULL, &hkey);
+	//if (RegCreateKeyW(hkey, L"MyKey", &hMyKey) == ERROR_SUCCESS)
+	//{
+	//	//MessageBoxW(NULL, L"Раздел создан", L"YES", MB_OK);
+	//}
+	//if (RegSetValueW(hMyKey, NULL, REG_SZ, L"Message", 8 * sizeof(WCHAR)) == ERROR_SUCCESS)
+	//{
+	//	WCHAR text [256];
+	//	DWORD size = sizeof(WCHAR) * 256;
+	//	if (RegGetValueW(hMyKey, NULL, NULL, RRF_RT_REG_SZ, NULL, text,&size ) == ERROR_SUCCESS)
+	//	{
+	//		MessageBoxW(NULL, text, L"YES", MB_OK);
+	//	}
 
-	}
-	return 0;
+	//}
+	//return 0;
+}
+
+
+
+
+void GetSettings() {
+
+}
+
+void SetFontSize() {
+
+}
+
+void SetFontColor() {
+
+}
+
+void SetBackground() {
+
+}
+
+void SetFontType() {
 
 }
